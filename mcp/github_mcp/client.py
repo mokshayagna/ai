82c14d93bfa9567e86890af44bedb3f3a061ec15
mokshayagna <mokshayagna09@gmail.com>
2026-06-07 11:11:32 +0530

    adding git client

---
+++ mcp/github_mcp/client.py
@@ -0,0 +1,43 @@
+import asyncio
+import os
+
+from mcp import ClientSession
+from mcp.client.stdio import stdio_client, StdioServerParameters
+
+async def main():
+
+    server_params = StdioServerParameters(
+        command="docker",
+        args=[
+            "run",
+            "-i",
+            "--rm",
+            "-e",
+            "GITHUB_PERSONAL_ACCESS_TOKEN",
+            "ghcr.io/github/github-mcp-server"
+        ],
+        env={
+            "GITHUB_PERSONAL_ACCESS_TOKEN":
+                os.environ["GITHUB_PERSONAL_ACCESS_TOKEN"]
+        }
+    )
+
+    async with stdio_client(server_params) as (read_stream, write_stream):
+
+        async with ClientSession(
+            read_stream,
+            write_stream
+        ) as session:
+
+            await session.initialize()
+
+            tools = await session.list_tools()
+
+            print("\nAvailable Tools:\n")
+
+            for tool in tools.tools:
+                print(tool.name)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
