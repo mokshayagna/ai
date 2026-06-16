a078139ef583b98f6ad80e7ceb79346f27c7548b
mokshayagna <mokshayagna09@gmail.com>
2026-06-16 12:18:03 +0530

    commiting modified code of list_tools

--- mcp/jira/mcp_list_tools.py
+++ mcp/jira/mcp_list_tools.py
@@ -20,8 +20,6 @@ async def MCPS_List_Tools(server_url: str):
 
 async def main():
     MCP_SERVER_URL = "http://127.0.0.1:3333/mcp"
-    # MCP_SERVER_URL = "https://backend.composio.dev/v3/mcp/6d417ba6-ccfc-41cd-9766-ad23cb1eb0f5/mcp?user_id=pg-test-e6b3d02f-0015-4dbe-96d1-267d0fe8a0f4"            
-    # MCP_SERVER_URL = "https://backend.composio.dev/v3/mcp/1cdbe274-c250-4f48-a273-37462152d796/mcp?user_id=pg-test-e6b3d02f-0015-4dbe-96d1-267d0fe8a0f4"
     await MCPS_List_Tools(MCP_SERVER_URL)
 
 if __name__ == "__main__":
